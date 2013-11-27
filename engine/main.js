09b57c84cb6a931847c72dcdcf40280024e34cb5
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2013-11-27 13:20:12 -0500

    Added "autoLoad" setting to global config, so a game can be loaded apart from the page's "load" event.

--- engine/main.js
+++ engine/main.js
@@ -12,13 +12,20 @@ Main.js creates the game object. Main.js is called on the window 'load' event.
 })(window);
 
 window.addEventListener('load', function(){
-//	window.console = {log:function(txt){document.title += txt;}}; //test code for android
-//	document.title = '';
+	if(platformer.settings && platformer.settings.global && (platformer.settings.global.autoLoad !== false)){
+		platformer.loadGame();
+	}
+}, false);
 
-	platformer.game = new platformer.classes.game(platformer.settings, function(game){});
+// This creates the game, typically immediately once the page is loaded. If the game should not appear on page load, global setting "autoLoad" needs to be set to `false` and this method must be called independently.
+platformer.loadGame = function(settings, onFinishedLoading){
+	var gameDefinition = settings || platformer.settings;
+	
+	platformer.game = new platformer.classes.game(gameDefinition, onFinishedLoading);
+	platformer.settings = gameDefinition;
 	createjs.Ticker.timingMode = 'raf';
-	createjs.Ticker.setFPS(platformer.settings.global.fps || 60);
+	createjs.Ticker.setFPS(gameDefinition.global.fps || 60);
 	createjs.Ticker.addEventListener("tick", function(e){
 		platformer.game.tick(e);
 	});
-}, false);
+};
