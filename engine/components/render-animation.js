eee0b375d855e82c244b6021da6829b3ac037c17
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2014-05-02 09:24:33 -0400

    Adding "render-animation" and "render-image" pointers for backwards compatibility.

---
+++ engine/components/render-animation.js
@@ -0,0 +1,19 @@
+/**
+# COMPONENT **render-animation**
+This component has been deprecated and is retained here for backwards compatibility. Use a "render-sprite" component instead.
+
+Requires: ["render-sprite"]
+*/
+(function(){
+	
+	console.warn('The "render-animation" component has been deprecated. Use a "render-sprite" component instead.');
+	
+	platformer.components['render-animation'] = platformer.components['render-sprite'];
+
+	if(!platformer.components['render-animation']){
+		//step back and let "render-sprite" load first
+		setTimeout(function(){
+			platformer.components['render-animation'] = platformer.components['render-sprite'];
+		}, 1);
+	}
+})();
---
+++ engine/components/render-image.js
@@ -0,0 +1,19 @@
+/**
+# COMPONENT **render-image**
+This component has been deprecated and is retained here for backwards compatibility. Use a "render-sprite" component instead.
+
+Requires: ["render-sprite"]
+*/
+(function(){
+	
+	console.warn('The "render-image" component has been deprecated. Use a "render-sprite" component instead.');
+	
+	platformer.components['render-image'] = platformer.components['render-sprite'];
+
+	if(!platformer.components['render-image']){
+		//step back and let "render-sprite" load first
+		setTimeout(function(){
+			platformer.components['render-image'] = platformer.components['render-sprite'];
+		}, 1);
+	}
+})();
