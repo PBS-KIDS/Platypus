7190763cd47bd78dbd97e9469e1eb9e45311b0be
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2015-05-27 13:04:42 -0400

    Updated `ai-chaser' to use the `mover` component.

--- engine/components/ai-chaser.js
+++ engine/components/ai-chaser.js
@@ -1,92 +1,152 @@
 /**
-# COMPONENT **ai-chaser**
-This component acts as a simple AI that will chase another entity.
-
-## Dependencies:
-- [[collision-basic]] (on entity) - This component listens for collision messages on the entity.
-- [[logic-angular-movement]] (on entity) - This component triggers messages for this component which moves the entity.
-- [[handler-ai]] (on entity's parent) - This component listens for an ai "tick" message to orderly perform its control logic.
-
-## Messages
-
-### Listens for:
-- **handle-ai** - This AI listens for a step message triggered by its entity parent in order to perform its logic on each tick.
-- **set-target** - On receiving this message, the component will change its target and begin chasing the new entity.
-  - @param message ([[Entity]]) - Sets this entity's target to the provided entity.
-- **start-chasing** - On receiving this message, the component will begin chasing the entity.
-- **stop-chasing** - On receiving this message, the component will cease chasing the entity.
-
-### Local Broadcasts:
-- **stop** - Triggered by this component before triggering another direction.
-- **set-angle** - Triggered to direct the entity toward the entity it is chasing.
-- **move** - Triggered once a target is set to begin moving toward the target.
-
-## JSON Definition:
-    {
-      "type": "ai-chaser"
-    }
-*/
-/*global platformer */
+ * This component acts as a simple AI that will chase another entity.
+ *
+ * @class "ai-chaser" Component
+ * @uses Component
+ */
+/*global console, platformer */
 (function () {
     "use strict";
+    
+    var tempVect = new platformer.Vector();
 
     return platformer.createComponentClass({
         
         id: 'ai-chaser',
         
+        properties: {
+            /**
+             * Sets whether the speed property should enact acceleration upon the entity rather than velocity.
+             * 
+             * @property accelerate
+             * @type boolean
+             * @default false
+             */
+            accelerate: false,
+            
+            /**
+             * Whether the entity is in a chasing state.
+             * 
+             * @property chasing
+             * @type boolean
+             * @default true
+             */
+            chasing: true
+        },
+        
+        publicProperties: {
+            /**
+             * Sets the velocity of the entity. This property is accessible on the entity as `entity.speed`.
+             * 
+             * @property speed
+             * @type number
+             * @default 0.3
+             */
+            speed: 0.3
+        },
+        
         constructor: function (definition) {
             this.target = this.owner.target || null;
-            this.piOverTwo = Math.PI / 2;
-            this.prevAngle = 0;
-            this.chasing = true;
-            this.offset = {x: 0, y: 0};
+            this.offset = new platformer.Vector(0, 0);
         },
 
         events: {// These are messages that this component listens for
+            /**
+             * This component listens for this event to initialize movement.
+             * 
+             * @method 'load'
+             */
+            "load": function () {
+                if (!this.owner.addMover) {
+                    console.warn('The "ai-chaser" component requires a "mover" component to function correctly.');
+                    return;
+                }
+                
+                this.direction = this.owner.addMover({
+                    vector: [this.speed, 0, 0],
+                    event: "chase",
+                    accelerator: this.accelerate
+                }).vector;
+            },
+        
+            /**
+             * This AI listens for a step message triggered by its entity parent in order to perform its logic on each tick.
+             * 
+             * @method 'handle-ai'
+             */
             "handle-ai": function () {
+                var v = tempVect,
+                    m = 0,
+                    c = false;
+
                 if (this.target && this.chasing) {
-                    //figure out angle
-                    this.owner.trigger('move');
-                    var angle = 0,
-                        dX = this.target.x + this.offset.x - this.owner.x,
-                        dY = this.target.y + this.offset.y - this.owner.y;
-                    if (dX === 0) {
-                        if (dY > 0) {
-                            angle = this.piOverTwo;
-                        } else if (dY < 0) {
-                            angle = -this.piOverTwo;
-                        } else {
-                            angle = this.prevAngle;
-                            this.owner.trigger('stop');
-                        }
-                    } else {
-                        angle = Math.atan(dY / dX);
-                        if (dX < 0) {
-                            angle = Math.PI + angle;
-                        }
+                    v.set(this.offset).add(this.target.position).subtractVector(this.owner.position);
+                    m = v.magnitude(2);
+
+                    if (m) {
+                        c = true;
+                        this.direction.set(v).normalize().multiply(this.speed);
                     }
-                    this.owner.trigger('set-angle', angle);
-                    this.prevAngle = angle;
-                } else {
-                    this.owner.trigger('stop');
+                }
+                
+                if (c !== this.owner.state.chasing) {
+                    this.owner.state.chasing = c;
+                    /**
+                     * This event is triggered whenever the entity begins chasing another entity or stops chasing another entity.
+                     * 
+                     * @event 'chase'
+                     * @param chasing {boolean} Whether the entity is chasing another entity.
+                     */
+                    this.owner.triggerEvent('chase', c);
                 }
             },
+            
+            /**
+             * On receiving this message, the component will change its target and begin chasing the new entity.
+             * 
+             * @method 'set-target'
+             * @param entity {Entity} Sets this entity's target to the provided entity.
+             */
             "set-target": function (entity) {
                 this.target = entity;
                 this.offset.x = 0;
                 this.offset.y = 0;
             },
+            
+            /**
+             * On receiving this message, the component will change its target offset.
+             * 
+             * @method 'set-target-offset'
+             * @param offset {Object|Vector} Sets the chased entity's offset to the provided offset.
+             * @param offset.x {number} The offset along the x-axis.
+             * @param offset.y {number} The offset along the y-axis.
+             */
             "set-target-offset": function (offset) {
                 this.offset.x = offset.x;
                 this.offset.y = offset.y;
             },
-            "start-chasing": function () {
+            
+            /**
+             * On receiving this message, the component will begin chasing the entity.
+             * 
+             * @method 'start-chasing'
+             * @param [entity] {Entity} Sets the entity if it's provided.
+             */
+            "start-chasing": function (entity) {
+                if (entity) {
+                    this.target = entity;
+                }
                 this.chasing = true;
             },
+            
+            /**
+             * On receiving this message, the component will cease chasing the entity.
+             * 
+             * @method 'stop-chasing'
+             */
             "stop-chasing": function () {
                 this.chasing = false;
             }
-                   
         },
         
         methods: {// These are methods that are called on the component
@@ -94,6 +154,5 @@ This component acts as a simple AI that will chase another entity.
                 this.target = null;
             }
         }
-        
     });
 }());
