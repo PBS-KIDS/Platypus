7f50445a5ea820a29599c0e068a291f329cbe659
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2015-08-04 16:06:52 -0400

    Updated Counter documentation.

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,61 +1,101 @@
 /**
-# COMPONENT **Counter**
-A simple component that keeps count of something and sends messages each time the count changes. Can also have a total. When it does it will display 'count / total'.
-
-## Messages
-
-### Listens for:
-- **increment-count** - Increments the count by 1.
-- **change-count** - Changes the count to the given value.
-  - @param data.count (number) - The new count value.
-- **change-total** - Changes the total to the given value.
-  - @param data.total (number) - The new total value.
-
-### Local Broadcasts:
-- **update-content** - A call used to notify other components that the count or total has changed.
-  - @param number - The count.
-  
-## JSON Definition
-    {
-      "type": "Counter"
-    }
-*/
+ * A simple component that keeps count of something and sends messages each time the count changes. Can also have a total. When it does it will display 'count / total'.
+ * 
+ * @namespace platypus.components
+ * @class Counter
+ * @uses Component
+ */
 /*global platypus */
 (function () {
     "use strict";
     
     return platypus.createComponentClass({
+
         id: 'Counter',
+
+        publicProperties: {
+            /**
+             * A total the counter is incrementing toward.
+             * 
+             * @property total
+             * @type number
+             * @default 0
+             */
+            total: 0
+        },
+
         constructor: function (definition) {
             this.count = 0;
-            this.total = 0;
-            this.showTotal = definition.showTotal || false;
-            this.output = {
-                text: ''
-            };
+            this.lastTotal = 0;
+            this.lastCount = 0;
         },
+
         events: {
+            /**
+             * Each step, this component detects whether the count has changed and triggers an 'update-content' event if so.
+             * 
+             * @method 'handle-logic'
+             */
+            "handle-logic": function () {
+                var txt = '',
+                update  = false;
+                
+                if (this.total !== this.lastTotal) {
+                    this.lastTotal = this.total;
+                    update = true;
+                }
+                
+                if (this.count !== this.lastCount) {
+                    this.lastCount = this.count;
+                    update = true;
+                }
+                
+                if (update) {
+                    if (this.total) {
+                        txt = String(this.count) + "/" + String(this.total);
+                    } else {
+                        txt = String(this.count);
+                    }
+                    
+                    /**
+                     * A call used to notify other components that the count or total has changed.
+                     * 
+                     * @event 'update-content'
+                     * @param update.text {string} String describing the current count.
+                     */
+                    this.owner.triggerEvent('update-content', {
+                        text: txt
+                    });
+                }
+            },
+
+            /**
+             * Changes the total to the given value.
+             * 
+             * @method 'change-total'
+             * @param data.total {number} The new total value.
+             */
             "change-total": function (total) {
                 this.total = total;
-                this.updateText();
             },
+
+            /**
+             * Changes the count to the given value.
+             * 
+             * @method 'change-count'
+             * @param data.count {number} The new count value.
+             */
             "change-count": function (count) {
                 this.count = count;
-                this.updateText();
             },
+
+            /**
+             * Increments the count by 1.
+             * 
+             * @method 'increment-count'
+             */
             "increment-count": function () {
                 this.count += 1;
-                this.updateText();
-            }
-        },
-        methods: {
-            updateText: function () {
-                if (this.total) {
-                    this.output.text = String(this.count) + "/" + String(this.total);
-                } else {
-                    this.output.text = String(this.count);
-                }
-                this.owner.trigger('update-content', this.output);
             }
         }
     });
