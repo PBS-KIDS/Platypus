52ec8af983f8d8fe99a77bac161c74fc528e319d
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2015-08-11 16:19:07 -0400

    Made camera threshold default to a smaller value to prevent jerky movement.

--- src/components/camera.js
+++ src/components/camera.js
@@ -83,9 +83,9 @@
              * 
              * @property threshold
              * @type number
-             * @default 1
+             * @default 0.001
              **/
-            "threshold": 1,
+            "threshold": 0.001,
             
             /**
              * Whether, when following an entity, the camera should rotate to match the entity's orientation.
