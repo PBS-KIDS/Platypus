d234983a658fbcf6f5382f69517d5bb70223aea2
Derek Detweiler <derek.detweiler@gopherwoodstudios.com>
2015-10-21 09:32:08 -0400

    Added documentation for RandomEvents.

--- src/components/RandomEvents.js
+++ src/components/RandomEvents.js
@@ -1,28 +1,20 @@
 /**
-# COMPONENT **RandomEvents**
-This component listens for certain messages, picks a message from a related list of events, and triggers it. This is useful for adding random behaviors to an entity, such as having an entity say one thing from a list of audio clips.
-
-## Messages
-
-### Listens for:
-- **[Messages specified in definition]** - Listens for messages and on receiving them, triggers a random message from the JSON-defined list.
-  - @param message (any) - If a message object comes in with the event, it is passed along with the selected random message.
-
-### Local Broadcasts:
-- **[Messages specified in definition]** - On receiving a recognized message, this component triggers one message from a JSON-defined list.
-  - @param message (any) - If a message object comes in with the triggered event, it is passed along with the selected random message.
-
-## JSON Definition
-    {
-      "type": "RandomEvents"
-      
-      "events"{
-      // This is a key/value list of events to listen for, with each event mapping to an array of events to pick from.
-      
-        "make-sound": ["scream", "whisper", "talk"]
-        //on the component receiving the "make-sound" message, it will trigger one of the three possible messages listed here.
-      }
-    }
+ * This component listens for certain messages, picks a message from a related list of events, and triggers it. This is useful for adding random behaviors to an entity, such as having an entity say one thing from a list of audio clips. For example, defining this component on an Entity may look like this:
+ * 
+ *     {
+ *       "type": "RandomEvents",
+ *       
+ *       "events": {
+ *       // This is a key/value list of events to listen for, with each event mapping to an array of events to pick from.
+ *       
+ *         "make-sound": ["scream", "whisper", "talk"]
+ *         //on the component receiving the "make-sound" message, it will trigger one of the three possible messages listed here.
+ *       }
+ *     }
+ *     
+ * @namespace platypus.components
+ * @class RandomEvents
+ * @uses Component
 */
 /*global platypus */
 (function () {
