7bd996e91b07b83e72f4c8e3652404cbf072faa1
Derek Detweiler <derek.detweiler@gmail.com>
2016-11-15 10:38:49 -0500

    Removed some graphics API calls. Also made remaining more resilient to handle other libraries defining their own (like pixi-animate.js)

--- src/mixins/PIXI.Graphics.js
+++ src/mixins/PIXI.Graphics.js
@@ -1,45 +1,43 @@
 /**
  * This adds support for a tiny API to PIXI.Graphics similar to the CreateJS Graphics API. This is used for backwards support for RenderSprite masks.
  */
-/* global include, platypus */
+/* global include */
 (function () {
     'use strict';
     
     var Graphics = include('PIXI.Graphics'),
-        debug = function (param) {
-            platypus.debug.olive('"' + param + '" is not an available PIXI.Graphics method.');
-            return this;
-        },
         gfx = Graphics.prototype;
     
-    gfx.a  = gfx.arc;
-    gfx.at = gfx.arcTo;
-    gfx.bt = gfx.bezierCurveTo;
-    gfx.c  = gfx.clear;
-    gfx.dc = gfx.drawCircle;
-    gfx.de = gfx.drawEllipse;
-    gfx.dr = gfx.drawRect;
-    gfx.ef = gfx.endFill;
-    gfx.f  = gfx.beginFill;
-    gfx.lt = gfx.lineTo;
-    gfx.mt = gfx.moveTo;
-    gfx.qt = gfx.quadraticCurveTo;
-    gfx.r  = gfx.drawRect;
-    gfx.rr = gfx.drawRoundedRect;
+    gfx.a  = gfx.a  || gfx.arc;
+    gfx.at = gfx.at || gfx.arcTo;
+    gfx.bt = gfx.bt || gfx.bezierCurveTo;
+    gfx.c  = gfx.c  || gfx.clear;
+    gfx.dc = gfx.dc || gfx.drawCircle;
+    gfx.de = gfx.de || gfx.drawEllipse;
+    gfx.dr = gfx.dr || gfx.drawRect;
+    gfx.ef = gfx.ef || gfx.endFill;
+    gfx.f  = gfx.f  || gfx.beginFill;
+    gfx.lt = gfx.lt || gfx.lineTo;
+    gfx.mt = gfx.mt || gfx.moveTo;
+    gfx.qt = gfx.qt || gfx.quadraticCurveTo;
+    gfx.r  = gfx.r  || gfx.drawRect;
+    gfx.rr = gfx.rr || gfx.drawRoundedRect;
     
-    gfx.cp = debug.bind("cp");
-    gfx.lf = debug.bind("lf");
-    gfx.rf = debug.bind("rf");
-    gfx.bf = debug.bind("bf");
-    gfx.ss = debug.bind("ss");
-    gfx.sd = debug.bind("sd");
-    gfx.s  = debug.bind("s");
-    gfx.ls = debug.bind("ls");
-    gfx.rs = debug.bind("rs");
-    gfx.bs = debug.bind("bs");
-    gfx.es = debug.bind("es");
-    gfx.rc = debug.bind("rc");
-    gfx.dp = debug.bind("dp");
-    gfx.p  = debug.bind("p");
+    /* Other CreateJS shortcuts that are unsupported by PIXI
+    gfx.cp
+    gfx.lf
+    gfx.rf
+    gfx.bf
+    gfx.ss
+    gfx.sd
+    gfx.s
+    gfx.ls
+    gfx.rs
+    gfx.bs
+    gfx.es
+    gfx.rc
+    gfx.dp
+    gfx.p
+    */
     
 } ());
