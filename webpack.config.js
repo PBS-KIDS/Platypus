906d36f42ca91b4dd7bff79c062a511afd2cb100
Derek Detweiler <derek.detweiler@gmail.com>
2019-11-06 09:06:55 -0500

    Attempt to separate large libs from inclusion.

--- webpack.config.js
+++ webpack.config.js
@@ -8,22 +8,19 @@ module.exports = env => {
         mode: mode,
         resolve: {
             alias: {
-                config: path.join(__dirname, 'src', 'config', mode),
-                createjs: path.join(__dirname, 'node_modules/createjs/builds/1.0.0/createjs.js')
+                config: path.join(__dirname, 'src', 'config', mode)
             }
         },
         output: {
             path: path.resolve(__dirname, 'lib'),
             filename: 'platypus.js',
             library: 'platypus'
-        }/*, TODO: Should externalize PIXI and SpringRoll at some point.
+        },
         externals: {
-            PIXI: {
-                commonjs: 'PIXI',
-                commonjs2: 'PIXI',
-                amd: 'PIXI',
-                root: 'PIXI'
-            }
-        }*/
+            "@tweenjs/tween.js": "@tweenjs/tween.js",
+            "pixi-sound": "pixi-sound",
+            "pixi.js": "pixi.js",
+            "springroll": "springroll"
+        }
     };
 };
