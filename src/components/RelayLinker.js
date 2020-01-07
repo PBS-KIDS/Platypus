8a25fd0624c49829d1ff94beb0cb7af0df752536
Derek Detweiler <derek.detweiler@gmail.com>
2020-01-06 20:33:02 -0500

    Simplified RelayLinker and removed "link-entity" and related events.

--- src/components/RelayLinker.js
+++ src/components/RelayLinker.js
@@ -5,179 +5,89 @@
  * @class RelayLinker
  * @uses platypus.Component
  */
-import {arrayCache, greenSplice} from '../utils/array.js';
+/* global platypus */
 import createComponentClass from '../factory.js';
+import {greenSplice} from '../utils/array.js';
 
-export default (function () {
-    var broadcast = function () {
-        var i = 0,
-            link = null;
-        
-        for (i = 0; i < this.links.length; i++) {
-            link = this.links[i];
-            link.trigger.apply(link, arguments);
+const
+    channels = {},
+    broadcast = function (...args) {
+        const channel = this.channel;
+
+        for (let i = 0; i < channel.length; i++) {
+            channel[i].trigger(...args);
         }
     };
 
-    return createComponentClass({
-        id: 'RelayLinker',
+export default createComponentClass({
+    id: 'RelayLinker',
 
-        properties: {
-            /**
-             * The id that defines the 'channel' the linkers are talking on. This should be matched on the entity/entities you want to talk between.
-             *
-             * @property linkId
-             * @type String
-             * @default 'linked'
-             */
-            linkId: 'linked',
-            
-            /**
-             * This is an object of key/value pairs. The keys are events this component is listening for locally, the value is the event to be broadcast to its linked entities. The value can also be an array of events to be fired on linked entities.
-             *
-             *      "events": {
-             *          "sleeping": "good-night",
-             *          "awake": ["alarm", "get-up"]
-             *      }
-             *
-             * @property events
-             * @type Object
-             * @default null
-             */
-            events: null
-        },
-
-        initialize: function () {
-            var i = 0,
-                event = '';
-
-            if (this.events) {
-                for (event in this.events) {
-                    if (this.events.hasOwnProperty(event)) {
-                        this.addEventListener(event, broadcast.bind(this, this.events[event]));
-                    }
-                }
-            }
-            
-            if (!this.owner.linkId) {
-                this.owner.linkId = this.linkId;
-            }
-            
-            this.addEventListener('to-' + this.linkId + '-entities', broadcast.bind(this, 'from-' + this.linkId + '-entities'));
-            this.addEventListener('from-' + this.linkId + '-entities', function (resp) {
-                this.owner.trigger(resp.message, resp.value, resp.debug);
-            }.bind(this));
-            
-            this.links = arrayCache.setUp();
-            
-            if (this.owner.linkEntities) {
-                for (i = 0; i < this.owner.linkEntities.length; i++) {
-                    this.links.push(this.owner.linkEntities[i]);
-                }
-            }
-            
-            this.message = {
-                message: '',
-                value: null
-            };
-            this.linkMessage = {
-                entity: this.owner,
-                linkId: this.linkId,
-                reciprocate: false
-            };
-
-            this.connected = false;
-            this.addEventListener('prepare-logic', () => {
-                if (!this.connected) {
-                    this.attemptConnection();
-                }
-                if (this.connected) {
-                    this.removeEventListener('prepare-logic');
-                }
-            });
-        },
+    properties: {
+        /**
+         * The id that defines the 'channel' the linkers are talking on. This should be matched on the entity/entities you want to talk between.
+         *
+         * @property linkId
+         * @type String
+         * @default 'linked'
+         */
+        linkId: 'linked',
         
-        events: {
-            /**
-             * Called when the object is added to its parent, on receiving this message, the component tries to link itself with objects with the same link id.
-             *
-             * @method 'load'
-             */
-            "load": function () {
-                this.attemptConnection();
-            },
+        /**
+         * This is an object of key/value pairs. The keys are events this component is listening for locally, the value is the event to be broadcast to its linked entities. The value can also be an array of events to be fired on linked entities.
+         *
+         *      "events": {
+         *          "sleeping": "good-night",
+         *          "awake": ["alarm", "get-up"]
+         *      }
+         *
+         * @property events
+         * @type Object
+         * @default null
+         */
+        events: null
+    },
 
-            /**
-             * Called when the object is added to its parent, on receiving this message, the component tries to link itself with objects with the same link id.
-             *
-             * @method 'layer-connected'
-             */
-            "layer-connected": function () {
-                if (!this.connected) {
-                    this.attemptConnection();
+    initialize: function () {
+        if (this.events) {
+            for (const event in this.events) {
+                if (this.events.hasOwnProperty(event)) {
+                    this.addEventListener(event, broadcast.bind(this, this.events[event]));
                 }
-            },
-            
-            /**
-             * On receiving this message, this component checks the linkId of the requesting entity and adds it to its list of connections if it matches.
-             *
-             * @method 'link-entity'
-             * @param toLink {platypus.Entity} The enquiring entity.
-             */
-            "link-entity": function (toLink) {
-                if ((toLink.linkId === this.linkId) && (toLink.entity !== this.owner)) {
-                    // Make sure this link is not already in place
-                    if (this.links.indexOf(toLink.entity) === -1) {
-                        this.links.push(toLink.entity);
-                        if (toLink.reciprocate) {
-                            this.linkMessage.reciprocate = false;
-                            toLink.entity.triggerEvent('link-entity', this.linkMessage);
-                        }
-                    }
-                }
-            },
-            
-            /**
-             * Removes the requesting entity from this component's list of linked entities and no farther messages will be transmitted.
-             *
-             * @method 'unlink-entity'
-             * @param toUnlink {platypus.Entity} The enquiring entity.
-             */
-            "unlink-entity": function (toUnlink) {
-                var i = this.links.indexOf(toUnlink.entity);
-                
-                if (i >= 0) {
-                    greenSplice(this.links, i);
-                }
-            }
-        },
-        
-        methods: {
-            attemptConnection: function () {
-                var grandparent = this.owner.parent;
-
-                while (grandparent.parent) {
-                    if (grandparent.loadingComponents) {
-                        // Bail because we're not connected to the rest of the scene yet.
-                        return;
-                    }
-                    grandparent = grandparent.parent;
-                }
-                this.linkMessage.reciprocate = true;
-                grandparent.triggerOnChildren('link-entity', this.linkMessage);
-                this.connected = true;
-            },
-
-            destroy: function () {
-                var i = 0;
-                
-                for (i = 0; i < this.links.length; i++) {
-                    this.links[i].triggerEvent('unlink-entity', this.linkMessage);
-                }
-                arrayCache.recycle(this.links);
-                this.links = null;
-                this.events = null;
             }
         }
-    });
-}());
+        
+        if (!this.owner.linkId) {
+            this.owner.linkId = this.linkId;
+        }
+        
+        // Connect channel, or create if it doesn't exist.
+        this.channel = channels[this.linkId];
+        if (!this.channel) {
+            this.channel = channels[this.linkId] = [];
+        }
+    },
+    
+    events: {
+        /**
+         * Called when the object is added to its parent, on receiving this message, the component links itself with objects with the same link id.
+         *
+         * @method 'load'
+         */
+        "load": function () {
+            this.channel.push(this.owner);
+        }
+    },
+    
+    methods: {
+        destroy: function () {
+            const index = this.channel.indexOf(this.owner);
+
+            if (index >= 0) {
+                greenSplice(this.channel, index);
+            } else {
+                platypus.debug.warn('RelayLinker: Component destroyed, but entity not found on channel.');
+            }
+            this.events = null;
+        }
+    }
+});
