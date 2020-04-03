9ae942f361d9e63b41e0597c33de1c37af4f8633
Derek Detweiler <derek.detweiler@gmail.com>
2020-04-03 07:21:14 -0400

    Added documentation for LogicTimer.

--- src/components/LogicTimer.js
+++ src/components/LogicTimer.js
@@ -1,46 +1,10 @@
 /**
-# COMPONENT **LogicTimer**
-A timer that can used to trigger events. The timer can increment and decrement. It can be an interval timer, going off over and over. Has a max time which it will not exceed by default this is 1 hour.
-
-## Dependencies
-- [[Handler-Logic]] (on entity's parent) - This component listens for a "handle-logic" message to update the timer.
-
-## Messages
-
-### Listens for:
-- **handle-logic** - Handles the update for the timer. Increments or decrements the current time. If it's hit the max it stops the timer at the max. If it hits the alarm it sets it off. Sends an update message indicating the timer's current time for other components to use.
-  - @param data.delta (number) - The time passed since the last tick.
-- **set** - Set the time.
-  - @param data.time (number) - The new value for the time.
-- **start** - Start the timer counting.
-- **stop** - Stop the timer counting.
-
-### Local Broadcasts:
-- **[alarm message from definition]** - The definition.alarm value from the JSON definition is used as the message id. It's sent when the alarm goes off.
-- **[update message from definition]** - The definition.update value from the JSON definition is used as the message id. It's sent every 'handle-logic' tick.
-  - @param message.time (number) - The current time value for the timer.
-
-## JSON Definition
-    {
-      "type": "LogicTimer",
-      "time" : 0,
-      //Optional - The starting time for the timer. Defaults to 0.
-      "alarmTime" : 10000,
-      //Optional - The time when the alarm will trigger the alarm message. Defaults to undefined, which never triggers the alarm.
-      "isInterval" : false,
-      //Optional - Whether or not the alarm fires at intervals of the alarmTime. Defaults to false.
-      "alarmMessage" : "ding",
-      //Optional - The message sent when the alarm goes off. Defaults to an empty string.
-      "updateMessage" : "",
-      //Optional - The message sent when the timer updates. Defaults to an empty string.
-      "on" : true,
-      //Optional - Whether the alarm starts on. Defaults to true.
-      "isIncrementing" : true,
-      //Optional - Whether the timer is incrementing or decrementing. If the value is false it is decrementing. Defaults to true.
-      "maxTime" : 3600000
-      //Optional - The max value, positive or negative, that the timer will count to. At which it stops counting. Default to 3600000 which equals an hour.
-    }
-*/
+ * A timer that can used to trigger events. The timer can increment and decrement. It can be an interval timer, going off over and over. Has a max time which it will not exceed by default this is 1 hour.
+ *
+ * @namespace platypus.components
+ * @class LogicTimer
+ * @uses platypus.Component
+ */
 import createComponentClass from '../factory.js';
 
 export default (function () {
@@ -48,29 +12,106 @@ export default (function () {
         id: 'LogicTimer',
 
         properties: {
+            /**
+             * The starting time for the timer.
+             *
+             * @property time
+             * @type Number
+             * @default 0
+             */
             time: 0,
+
+            /**
+             * The time when the alarm will trigger the alarm message. Defaults to `0``, which never triggers the alarm.
+             *
+             * @property alarmTime
+             * @type Number
+             * @default 0
+             */
             alarmTime: 0,
+
+            /**
+             * Whether or not the alarm fires at intervals of the alarmTime.
+             *
+             * @property isInterval
+             * @type Boolean
+             * @default false
+             */
             isInterval: false,
+
+            /**
+             * The message sent when the alarm goes off.
+             *
+             * @property alarmMessage
+             * @type String
+             * @default ''
+             */
             alarmMessage: '',
+
+            /**
+             * The message sent when the timer updates.
+             *
+             * @property updateMessage
+             * @type String
+             * @default ''
+             */
             updateMessage: '',
+
+            /**
+             * Whether the alarm starts on.
+             *
+             * @property isOn
+             * @type Boolean
+             * @default true
+             */
             isOn: true,
+
+            /**
+             * Whether the timer is incrementing or decrementing. If the value is `false` it is decrementing.
+             *
+             * @property isIncrementing
+             * @type Boolean
+             * @default true
+             */
             isIncrementing: true,
-            maxTime: 3600000, //Max time is 1hr by default.
+
+            /**
+             * The max value in MS, positive or negative, that the timer will count to. Once reached, it stops counting. Defaults to one hour.
+             *
+             * @property maxTime
+             * @type Number
+             * @default 3600000
+             */
+            maxTime: 3600000,
+
+            /**
+             * Whether this alarm should reset to full initial time if restarted after being stopped.
+             *
+             * @property resetOnStop
+             * @type Boolean
+             * @default false
+             */
             resetOnStop: false
         },
 
-        initialize: function (definition) {
+        initialize: function () {
             this.prevTime = this.time;
         },
 
         events: {
-            "handle-logic": function (data) {
+            /**
+             * This component listens for this event to perform countdown and trigger alarm as needed.
+             *
+             * @method 'handle-logic'
+             * @param {Number} tick.delta
+             */
+            "handle-logic": function (tick) {
                 if (this.isOn) {
                     this.prevTime = this.time;
                     if (this.isIncrementing) {
-                        this.time += data.delta;
+                        this.time += tick.delta;
                     } else {
-                        this.time -= data.delta;
+                        this.time -= tick.delta;
                     }
                     
                     if (Math.abs(this.time) > this.maxTime) {
@@ -103,12 +144,31 @@ export default (function () {
                 }
                 this.owner.trigger(this.updateMessage, {time: this.time});
             },
+
+            /**
+             * Sets time for alarm.
+             *
+             * @method 'set-timer'
+             * @param {Number} data.time Time to set for alarm.
+             */
             "set-timer": function (data) {
                 this.time = data.time;
             },
+
+            /**
+             * Starts the timer's countdown.
+             *
+             * @method 'start-timer'
+             */
             "start-timer": function () {
                 this.isOn = true;
             },
+
+            /**
+             * Stops the timer's countdown. If `resetOnStop` is `true`, resets timer.
+             *
+             * @method 'stop-timer'
+             */
             "stop-timer": function () {
                 this.isOn = false;
                 if (this.resetOnStop) {
