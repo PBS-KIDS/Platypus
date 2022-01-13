c472c6302ed125c2ac1831f6c802cf731b59e37d
Derek Detweiler <derek.detweiler@gmail.com>
2022-01-13 10:11:02 -0500

    Removed old grunt build process (apart from docs generation)

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,63 +14,6 @@ module.exports = function (grunt) {
         docsName: '<%= pkg.name %>_docs-<%= version %>',
         docsZip: "<%= docsName %>.zip",
 
-        // Setup Uglify for JS minification.
-        uglify: {
-            options: {
-                banner: "/*!\n * @license PLATYPUS v<%= version %>\n *\n * Distributed under the terms of the MIT license.\n * http://www.opensource.org/licenses/mit-license.html\n *\n * This notice shall be included in all copies or substantial portions of the Software.\n */\n\n",
-                preserveComments: "some",
-                compress: {
-                    global_defs: {
-                        "DEBUG": false
-                    }
-                }
-            },
-            build: {
-                files: {
-                    './build/output/<%= pkg.name.toLowerCase() %>.min.js': getConfigValue('platypus_source')
-                }
-            }
-        },
-
-        concat: {
-            options: {
-                separator: '',
-                process: function (src, filepath) {
-                    // Remove a few things from each file, they will be added back at the end.
-
-                    // Strip the license header.
-                    var file = src.replace(/^(\/\*\s)[\s\S]+?\*\//, "");
-
-                    // Strip namespace label
-                    file = file.replace(/\/\/\s*namespace:/, "");
-
-                    // Strip @module
-                    file = file.replace(/\/\*\*[\s\S]+?@module[\s\S]+?\*\//, "");
-
-                    // Clean up white space
-                    file = file.replace(/^\s*/, "");
-                    file = file.replace(/\s*$/, "");
-
-                    // Append on the class name
-                    file =
-                        "\n\n//##############################################################################\n"+
-                        "// " + path.basename(filepath) + "\n" +
-                        "//##############################################################################\n\n"+
-                        file;
-                    return file;
-                }
-            },
-            build: {
-                files: {
-                    './build/output/<%= pkg.name.toLowerCase() %>.combined.js': combineSource([{
-                        cwd: '',
-                        config: 'config.json',
-                        source: 'platypus_source'
-                    }])
-                }
-            }
-        },
-
         // Build docs using yuidoc
         yuidoc: {
             compile: {
@@ -111,7 +54,7 @@ module.exports = function (grunt) {
                     expand: true,
                     cwd: '<%= docsFolder %>',
                     src: '**',
-                    dest: getConfigValue('docs_out_path')
+                    dest: "docs/"
                 }]
             },
             src: {
@@ -122,55 +65,10 @@ module.exports = function (grunt) {
                     dest: './lib/'
                 }]
             }
-        },
-
-        updateversion: {
-            platypus: {
-                file: './src/platypus.js',
-                version: '<%= version %>'
-            }
-        },
-
-        clearversion: {
-            platypus: {
-                file: './src/platypus.js'
-            }
         }
     });
 
-    function getConfigValue (name) {
-        var config = grunt.file.readJSON('config.json');
-
-        grunt.config.set('buildConfig', config);
-
-        return config[name];
-    }
-
-    function combineSource (configs) {
-        var handle = function (item, index, array) {
-                array[index] = path.resolve(this.cwd, item);
-            },
-            i = 0,
-            json = null,
-            o = null,
-            sourcePaths = [],
-            sources = null;
-
-        // Pull out all the source paths.
-        for (i = 0; i < configs.length; i++) {
-            o = configs[i];
-            json = grunt.file.readJSON(path.resolve(o.cwd, o.config));
-            sources = json[o.source];
-            sources.forEach(handle.bind(o));
-            sourcePaths = sourcePaths.concat(sources);
-        }
-
-        return sourcePaths;
-    }
-
     // Load all the tasks we need
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -180,13 +78,6 @@ module.exports = function (grunt) {
     grunt.config.set('docsFolder', "./build/output/<%= docsName %>/");
 
     /**
-     * Build the docs using YUIdocs.
-     */
-    grunt.registerTask('docs', [
-        "yuidoc", "compress", "copy:docsSite"
-    ]);
-
-    /**
      * Sets out version to the version in package.json (defaults to NEXT)
      */
     grunt.registerTask('setVersion', function () {
@@ -194,43 +85,15 @@ module.exports = function (grunt) {
     });
 
     /**
-     * Task for exporting a next build.
-     *
-     */
-    grunt.registerTask('next', function () {
-        grunt.config("buildArgs", this.args || []);
-        grunt.config("concat.options.banner", "/*!\n * PLATYPUS v<%= pkg.version %>-next\n *\n */");
-        grunt.task.run(["updateversion", "combine", "uglify", "clearversion", "copy:src", "clearBuildArgs"]);
-    });
-
-    /**
-     * Task for exporting only the next lib.
-     *
-     */
-    grunt.registerTask('nextlib', [
-        "updateversion", "combine", "uglify", "clearversion", "copy:src"
-    ]);
-
-    /**
      * Task for exporting a release build (version based on package.json)
      *
      */
     grunt.registerTask('build', function () {
         grunt.config("buildArgs", this.args || []);
-        grunt.config("concat.options.banner", "/*!\n * PLATYPUS v<%= pkg.version %>\n *\n */");
-        grunt.task.run(["setVersion", "updateversion", "combine", "uglify", "clearversion", "docs", "copy:src", "updatebower", "copy:docsSite", "clearBuildArgs"]);
+        grunt.task.run(["setVersion", "yuidoc", "compress", "copy:docsSite", "clearBuildArgs"]);
     });
 
     grunt.registerTask('clearBuildArgs', function () {
         grunt.config("buildArgs", []);
     });
-
-    /**
-     * Task for exporting combined view.
-     *
-     */
-    grunt.registerTask('combine', 'Combine all source into a single, un-minified file.', [
-        "concat"
-    ]);
-
 };
